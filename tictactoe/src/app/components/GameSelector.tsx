69546e0eefda8f2c0a60ea16c0d0aa0932afdd9e
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-08 21:36:47 -0300

    fix deploy in vercel

--- tictactoe/src/app/components/GameSelector.tsx
+++ tictactoe/src/app/components/GameSelector.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image"
 
-export default function GameSelector({ name, link, icon }: any) {
+export default function GameSelector({ name, link, icon, key }: any) {
 
     return (
         <div className="border-2 p-4 text-xl flex flex-wrap space-between rounded-xl min-w-[200px]">
--- tictactoe/src/app/page.tsx
+++ tictactoe/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center h-[100vh] w-[100vw] bg-[#2e2e2e] text-4xl md:text-4xl">
       <h1 className= "mb-10 decoration-[#46B2F9] underline underline-offset-8">Bienvenido</h1>
-      <section className=' grid grid-cols-1 lg:grid-cols-3 justify-center items-center'>
+      <section className='grid grid-cols-1 lg:grid-cols-3 justify-center items-center place-items-center'>
         {
           games.map((game, index) => {
             return (
