7182be374a6f84c9579a43844c54b0a895e20ca2
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-09 18:14:55 -0300

    added back button

--- tictactoe/src/app/tictactoe/page.tsx
+++ tictactoe/src/app/tictactoe/page.tsx
@@ -7,6 +7,7 @@ import { TURNS } from "../constants/constants"
 import WinnerModal from "../components/WinnerModal"
 import confetti from "canvas-confetti"
 import { checkWinner } from "../logic/game"
+import ReturnButton from "../components/ReturnButton"
 
 export default function Home() {
     const [board, setBoard] = useState<any>(Array(9).fill(null))
@@ -63,6 +64,7 @@ export default function Home() {
   
     return (
       <main className="flex flex-col justify-center items-center h-[100vh] w-[100vw] bg-[#2e2e2e] text-4xl md:text-4xl">
+        <ReturnButton/>
         <h1 className='mb-10 decoration-[#46B2F9] underline underline-offset-8'>Tic Tac Toe</h1>
         <ButtonReset resetGame={resetGame}>Reset game</ButtonReset>
         <div className='grid grid-cols-3 grid-cols-3 font-light gap-2 bg-gradient-to-r from-[#b6bfff] via-[#5d61ff] to-[#00d4ff] rounded-xl'>
