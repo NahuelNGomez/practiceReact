4f135dc8dd8802f28f9eea15ad152583c609545a
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-09 18:16:38 -0300

    add link to back button

--- tictactoe/src/app/components/ReturnButton.tsx
+++ tictactoe/src/app/components/ReturnButton.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function ReturnButton(){
 
     return (
-
+        <Link href={"/"}>
         <button className="border rounded-3xl absolute left-0 top-0 p-1 md:py-3 md:px-8 m-4 text-sm md:text-lg bg-gradient-to-r from-[#ff6666] to-[#ffcccc] text-gray-800 font-bold md:w-[30%] w-[20%] max-w-[200px] ">Back</button>
+        </Link>
     )
 }
